49ca6d1e5e55a6a9c996351861e89795c0658d1e
Lucas Weiblen <lucasweiblen@gmail.com>
2020-10-28 15:34:54 -0300

    refactor: move sut creation to a factory method

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,26 @@
+import { CompareFieldsValidation } from './compare-fields-validation'
+import { InvalidParamError } from '../../errors'
+
+const makeSut = (): CompareFieldsValidation => {
+    return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('CompareFields Validation', () => {
+    test('should return a missing param error if validation fails', () => {
+        const sut = makeSut()
+        const error = sut.validate({
+            name: 'any_name',
+            fieldToCompare: 'wrong_value'
+        })
+        expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+    })
+
+    test('should not return if validation succeds', () => {
+        const sut = makeSut()
+        const error = sut.validate({
+            name: 'any_value',
+            fieldToCompareName: 'any_value'
+        })
+        expect(error).toBeFalsy()
+    })
+})
