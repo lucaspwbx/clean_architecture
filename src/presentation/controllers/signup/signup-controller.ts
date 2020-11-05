837d86ef3450ce6aae9e53dffa3f90240f571e2a
Lucas Weiblen <lucasweiblen@gmail.com>
2020-11-04 23:18:29 -0300

    chore: eslint

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -4,10 +4,10 @@ import { Validation } from '../../protocols/validation'
 import { EmailinUseError } from '../../../presentation/errors'
 
 export class SignUpController implements Controller {
-  constructor(private readonly addAccount: AddAccount, private readonly validation: Validation, private readonly authentication: Authentication) {
+  constructor (private readonly addAccount: AddAccount, private readonly validation: Validation, private readonly authentication: Authentication) {
   }
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) {
--- src/presentation/errors/email-in-use-error.ts
+++ src/presentation/errors/email-in-use-error.ts
@@ -1,6 +1,6 @@
 export class EmailinUseError extends Error {
-    constructor() {
-        super('The received email is already in use')
-        this.name = 'EmailInUseError'
-    }
+  constructor () {
+    super('The received email is already in use')
+    this.name = 'EmailInUseError'
+  }
 }
