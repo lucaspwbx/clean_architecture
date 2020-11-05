1c38581e99f05a28101de187930a1e2a362bbaee
Lucas Weiblen <lucasweiblen@gmail.com>
2020-11-04 23:16:15 -0300

    refactor: separe factories in folders

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,12 +1,10 @@
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
-import { LogControllerDecorator } from '../../../../main/decorators/log-controller-decorator'
 import { LoginController } from '../../../../presentation/controllers/login/login-controller'
 import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeDbAuthentication } from '../usecases/authentication/db-authentication-factory'
 import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLoginController = (): Controller => {
-  const loginController = new LoginController(makeDbAuthentication(), makeLoginValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  const controller = new LoginController(makeDbAuthentication(), makeLoginValidation())
+  return makeLogControllerDecorator(controller)
 }
