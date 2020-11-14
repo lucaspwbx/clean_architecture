cd3a4569b661379efd22313155a932b51b03994e
Lucas Weiblen <lucasweiblen@gmail.com>
2020-11-13 21:20:10 -0300

    chore: fix

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -5,12 +5,12 @@ import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
-  async add(surveyData: AddSurveyModel): Promise<void> {
+  async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
   }
 
-  async loadAll(): Promise<SurveyModel[]> {
+  async loadAll (): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     const surveys: SurveyModel[] = await surveyCollection.find().toArray()
     return surveys
