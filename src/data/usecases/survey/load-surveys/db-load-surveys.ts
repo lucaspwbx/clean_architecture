e320f361cdd6c18b4782d3ad6a2d04a72650dd22
Lucas Weiblen <lucasweiblen@gmail.com>
2020-11-18 15:05:47 -0300

    refactor: organize project files

---
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -0,0 +1,10 @@
+import { LoadSurveys, LoadSurveysRepository, SurveyModel } from '@/data/usecases/survey/load-surveys/db-load-surveys-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) { }
+
+  async load (): Promise<SurveyModel[]> {
+    const surveys = await this.loadSurveysRepository.loadAll()
+    return surveys
+  }
+}
